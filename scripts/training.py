476b7eb2a9103a64bcdb23670c4e9d6fd508610f
Yeray Martin-Ruisanchez <yeray.martin-ruisanchez@tum.de>
2025-07-15 18:14:04 +0200

    Adaptation to experiments and also CUDA usage

--- scripts/training.py
+++ scripts/training.py
@@ -13,7 +13,9 @@ Outputs:
 """
 #%%
 import os
-
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
+os.environ['WANDB_DIR'] = '/home/home/yeray_jonas/tumornetsolvers/wandb'
 import sys
 current_dir = os.path.dirname(os.path.abspath(__file__))  # scripts directory
 src_path = os.path.abspath(os.path.join(current_dir, '..', 'src'))
@@ -21,6 +23,8 @@ if src_path not in sys.path:
     sys.path.insert(0, src_path)
 
 import torch
+torch.cuda.empty_cache()
+torch.cuda.reset_peak_memory_stats()
 from TumorNetSolvers.training.updating_trainer import Trainer
 from batchgenerators.utilities.file_and_folder_operations import load_json
 from set_env import set_environment_variables
@@ -30,17 +34,27 @@ nnUNet_preprocessed = os.getenv('nnUNet_preprocessed')
 # ============ Configuration ============
 
 # Define the dataset and training configuration
-DATASET_NAME = 'Dataset700_Brain'  
+DATASET_NAME = 'Dataset300_Brain'  
 TRAINING_CONFIGURATION = '3d_fullres'  # '2d', '3d_lowres', '3d_fullres', etc.
-DEVICE = torch.device('cuda:0')  
+DEVICE = torch.device('cuda:0')
 
 # Define project and training parameters
 PROJECT_NAME = "NN-based-tumor-solvers"  # for wandb
-MODEL_NAME = "nnUnet"  # other options are 'TumorSurrogate' and 'ViT'
+MODEL_NAME = "ViT"  # other options are 'nnUnet', 'TumorSurrogate' and 'ViT'
 SIGNATURE = "10k"  # Unique signature for logging and reproducibility
 
 # Define experiments regarding insertion of parameters (mode and location)
-EXPERIMENTS = [['a', 'b_bottleneck'], ['c', 'b_bottleneck']]
+'''EXPERIMENTS = [
+    ['MLP', 'one_token'],
+    ['Linear', 'one_token'],
+    ['MLP', 'mul_token'],
+    ['MLP', 'embed_concat'],
+    ['Linear', 'embed_concat'],
+    ['MLP', 'embed_add'],
+    ['Linear', 'embed_add']]'''
+EXPERIMENTS = [['Linear', 'embed_concat']]
+
+#[['c', 'a_downsampling']]
 
 # ============ Load Training Plans and Dataset ============
 
@@ -56,23 +70,28 @@ dataset_json = load_json(DATASET_JSON_FILE)
 
 # ============ Initialize and Run Training ============
 
-# Initialize the nnU-Net trainer
-print("Initializing trainer...")
-trainer = Trainer(
-    plans=plan,
-    configuration=TRAINING_CONFIGURATION,
-    device=DEVICE,
-    signature=SIGNATURE,
-    fold='train_val_test',
-    model=MODEL_NAME,
-    dataset_json=dataset_json,
-    project_name=PROJECT_NAME,
-    experiments = EXPERIMENTS
-)
+for experiment in EXPERIMENTS:
+    # Initialize the nnU-Net trainer
+    print("Initializing trainer...")
+    trainer = Trainer(
+        plans=plan,
+        configuration=TRAINING_CONFIGURATION,
+        device=DEVICE,
+        signature=SIGNATURE,
+        fold='train_val_test',
+        model=MODEL_NAME,
+        dataset_json=dataset_json,
+        project_name=PROJECT_NAME,
+        experiments = experiment,
+        seed=12345
+    )
 
-# Run the training process
-print("Starting training...")
-trainer.run_training()
-print("Training complete.")
+    # Run the training process
+    print("Starting training...")
+    trainer.run_training()
+    if MODEL_NAME == "ViT":
+        print("Training complete." + f"[{MODEL_NAME}] Mode: {experiment[1]}, Method: {experiment[0]}")
+    else:
+        print("Training complete." + f"[{MODEL_NAME}] Loc: {experiment[1]}, Mode: {experiment[0]}")
 
 # %%
